45dd5b23a9574014548a05f83f9e51d6988073f9
akihirot <akihirot89@gmail.com>
2014-05-05 11:45:20 +0900

    Update Environment.bat

--- Environment.bat
+++ Environment.bat
@@ -1,16 +1,16 @@
-echo off
+@echo off
 reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v %1
 goto label_%ERRORLEVEL%
 
 :label_0
 echo This Key is already there
 for /f "tokens=1,2*" %%R in ('reg query "HKLM\SYSTEM\CurrentControlSet\Control\Session Manager\Environment" /v %1') do set VALUE=%%~T
-setx %1 %VALUE%;%2 /M
+setx -M %1 %VALUE%;%2
 goto end
 
 :label_1
 echo This Key does not exist yet
-setx %1 %2 /M
+setx -M %1 %2
 goto end
 
 :end
